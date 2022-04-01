e46c1e909b193afecddda0640a5b0aeb56da7cbe
0x5656 <96357775+0x5656@users.noreply.github.com>
2022-04-01 17:22:33 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+class anticrash {
+    constructor(logging, process){
+        if(!logging) logging = false;
+        if(typeof logging !== 'boolean') throw new TypeError(`[Anticrash] : logging must be a boolean`);
+        if(!process) throw new TypeError(`[Anticrash] : process must be renseigned`);
+        this.logging = logging;
+        this.process = process;
+        this.start();
+    }
+
+    async start(){
+        console.log(`[Anticrash] : Started`);
+
+        this.process.on('uncaughtException', (reason, p) => {
+            return;
+        });
+
+        this.process.on('uncaughtExceptionMonitor', (reason, p) => {
+            return
+        });
+
+        this.process.on('unhandledRejection', (reason, p)=>{
+            if(!this.logging) return;
+            let getNow = () => { return { time: new Date().toLocaleString("fr-FR", { timeZone: "Europe/Paris", hour12: false, hour: "2-digit", minute: "2-digit", second: "2-digit" }) } };
+            console.log(`${getNow().time} | [Anticrash] :`);
+            console.log(p);
+            return console.log(reason);
+        })
+
+    }
+
+}
+
+exports.anticrash = anticrash
