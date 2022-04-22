0226d11b97ebe620680e2d0bca6834c4105b9cad
0x5656 <96357775+0x5656@users.noreply.github.com>
2022-04-22 04:03:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,15 @@
 class Anticrash {
-    constructor(logging, process){
-        if(!logging) logging = false;
-        if(typeof logging !== 'boolean') throw new TypeError(`[Anticrash] : logging must be a boolean`);
+    constructor(process){
         if(!process) throw new TypeError(`[Anticrash] : process must be renseigned`);
-        this.logging = logging;
         this.process = process;
         this.start();
     }
 
     async start(){
-        console.log(`[Anticrash] : Started`);
-
-        this.process.on('uncaughtException', (reason, p) => {
-            return;
-        });
-
-        this.process.on('uncaughtExceptionMonitor', (reason, p) => {
-            return
-        });
-
-        this.process.on('unhandledRejection', (reason, p)=>{
-            if(!this.logging) return;
-            let getNow = () => { return { time: new Date().toLocaleString("fr-FR", { timeZone: "Europe/Paris", hour12: false, hour: "2-digit", minute: "2-digit", second: "2-digit" }) } };
-            console.log(`${getNow().time} | [Anticrash] :`);
-            console.log(p);
-            return console.log(reason);
-        })
-
+        this.process.on('uncaughtException', (err, origin) => { return });
+        this.process.on('unhandledRejection', (err, promise) => { return });
+        this.process.on('uncaughtExceptionMonitor', (err, origin) => { return });
+        this.process.on('multipleResolves', (type, pomise) => { return });
     }
 
 }
